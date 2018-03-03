8c64eab6641e1792514a0e72bdbc2e9b34fc3774
Nick Konecny <nkonecny@etsy.com>
2018-03-03 15:21:40 -0500

    working

--- MMM-CitiBike.js
+++ MMM-CitiBike.js
@@ -17,14 +17,6 @@ Module.register("MMM-CitiBike", {
 		return ["moment.js"];
 	},
 
-	// notificationReceived: function(notification, payload) {
-	//     console.log(notification);
-	//     if (notification === 'DOM_OBJECTS_CREATED') {
-	//         this.sendSocketNotification('GET_CITIBIKE_STATUS', self.config);
-	//     }
-	// },
-
-
 	defaults: {
 		fetchInterval: 15 * 1000,
 	},
@@ -46,29 +38,62 @@ Module.register("MMM-CitiBike", {
 
 	getDom: function() {
 		var container = document.createElement("div");
-		
+		var h2 = document.createElement("div");
+		h2.innerHTML = "CitiBike"
+		container.appendChild(h2);
+		container.appendChild(document.createElement("hr"));
 
 		
 		if (this.bikes) {
+			var table = document.createElement("table");
+			var headerRow = document.createElement("tr");
+			this.createHeader(headerRow, "Address");
+			this.createHeader(headerRow, "🚲 Left");		
+			this.createHeader(headerRow, "Updated");	
+			table.appendChild(headerRow);	
+			
 			this.bikes.forEach(bike => {
 				console.log(bike);
+				var tr = document.createElement("tr");
+
+				var td = document.createElement("td");
+				var td2 = document.createElement("td");
+				var td3 = document.createElement("td");
+
+				td.innerHTML = bike.name;
+				td.className = 'small';
+
+				td2.innerHTML = bike.free_bikes;
+				td2.style = "text-align:center";
+
+				td3.innerHTML = ` As of ${moment.unix(bike.extra.last_updated).fromNow()}`;
+				td3.className = 'xsmall';
+				
+				tr.appendChild(td);
+				tr.appendChild(td2);
+				tr.appendChild(td3);
+				table.appendChild(tr);
+				
+				
+				
 			});
-			
-			// h3.innerHTML = "Cash: $" + this.bankData.cash + "<br/>Credit: ($" + this.bankData.credit + ")";					
-			// var div = document.createElement("div");
-			// div.innerHTML = "Last updated: " + new Date(this.bankData.date);	
-			// div.style.fontSize = "14px";
-			// container.appendChild(h3);			
-			// container.appendChild(div);
+
+			container.appendChild(table);
+		
 		}
 		
 		return container;
 	},
 
-	socketNotificationReceived: function (notification, payload) {
-		console.log(payload);
+	createHeader: function(container, text) {
+		var header = document.createElement("th");		
+		header.innerHTML = text;
+		header.style ="text-align:center; padding-left:8px; padding-right:8px;";
+		container.appendChild(header);
+	},
+
+	socketNotificationReceived: function (notification, payload) {		
 		if (notification === 'CITIBIKE_DATA') {
-			console.log(payload);
 			this.bikes = payload.data;			
 			this.updateDom();
 		}
